d6d76fc1166d3269166c94a1d48d4d3e8e68cd51
Michael Blandford <mdblandford@gmail.com>
2018-06-19 20:36:31 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,5 +1,5 @@
 def line(katz_deli)
-  if katz_deli.length == 0 
+  if katz_deli.length == 0
     puts "The line is currently empty."
   else
     message = "The line is currently:"
@@ -11,12 +11,12 @@ def line(katz_deli)
 end
 
 def take_a_number(katz_deli,name)
-  katz_deli << name 
+  katz_deli << name
     puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
 
 def now_serving(katz_deli)
-  if katz_deli.length == 0 
+  if katz_deli.length == 0
     puts "There is nobody waiting to be served!"
   else
     puts "Currently serving #{katz_deli.shift}."
