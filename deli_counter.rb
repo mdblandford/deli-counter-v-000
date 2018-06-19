1171391abc41f3739153b3943724911513271f6b
Michael Blandford <mdblandford@gmail.com>
2018-06-19 20:36:26 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,7 +3,7 @@ def line(katz_deli)
     puts "The line is currently empty."
   else
     message = "The line is currently:"
-      katz_deli.each_with_index do |value, index|
+      katz_deli.each_with_index do |index, value|
       message += " #{index.to_i+1}. #{value}"
     end
    puts "#{message}"
